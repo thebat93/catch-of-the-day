301f1a643cd5f286df36181e8209afcb22a89e15
thebat93 <thebat93@sibmail.com>
2017-12-21 17:52:05 +0300

    Animating React Components

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,6 +1,7 @@
 // Список заказов
 import React from 'react';
 import { formatPrice } from '../helpers';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends React.Component {
     constructor() {
@@ -19,7 +20,18 @@ class Order extends React.Component {
         // если рыба есть, то рендерим элемент списка
         return (
             <li key={key}>
-                <span>{count}lbs {fish.name} {removeButton}</span>
+                <span>
+                    <CSSTransitionGroup
+                        component="span"
+                        className="count"
+                        transitionName="count"
+                        transitionEnterTimeout={250}
+                        transitionLeaveTimeout={250}
+                    >
+                        <span key={count}>{count}</span>
+                    </CSSTransitionGroup> 
+                    lbs {fish.name} {removeButton}
+                </span>
                 <span className="price">{formatPrice(count * fish.price)}</span>
             </li>
         )
@@ -38,13 +50,20 @@ class Order extends React.Component {
         return (
             <div className="order-wrap">
                 <h2>Your Order</h2>
-                <ul className="order">
+                { /* Применяем анимацию к списку */ }
+                <CSSTransitionGroup 
+                    className="order"
+                    component="ul"
+                    transitionName="order"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}
+                >
                     {orderIds.map(this.renderOrder)}
                     <li className="total">
                         <strong>Total:</strong>
                         {formatPrice(total)}
                     </li>
-                </ul>
+                </CSSTransitionGroup>
             </div>
         )
     }
