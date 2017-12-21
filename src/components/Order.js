552f6ff53914c0177decf2d70d3301d69c981378
thebat93 <thebat93@sibmail.com>
2017-12-21 12:34:36 +0300

    Displaying Order State with JSX

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,49 @@
+// Список заказов
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-    render() {
+    constructor() {
+        super();
+        this.renderOrder = this.renderOrder.bind(this);
+    }
+    renderOrder(key) { // функция для рендера каждого заказа из списка
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+
+        if (!fish || fish.status === 'unavailable') { // если рыбы нет или она была распродана
+            return <li key={key}>Sorry, {fish ? fish.name : 'the fish'} is no longer available</li>
+        } 
+        // если рыба есть, то рендерим элемент списка
         return (
-            <p>Order</p>
+            <li key={key}>
+                <span>{count}lbs {fish.name}</span>
+                <span className="price">{formatPrice(count * fish.price)}</span>
+            </li>
+        )
+    }
+    render() {
+        const orderIds = Object.keys(this.props.order); // положить все ID в массив
+        const total = orderIds.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status === 'available';
+            if (isAvailable) {
+                return prevTotal + (count * fish.price || 0);
+            }
+            return prevTotal;
+        }, 0); // итоговая сумма
+        return (
+            <div className="order-wrap">
+                <h2>Your Order</h2>
+                <ul className="order">
+                    {orderIds.map(this.renderOrder)}
+                    <li className="total">
+                        <strong>Total:</strong>
+                        {formatPrice(total)}
+                    </li>
+                </ul>
+            </div>
         )
     }
 }
